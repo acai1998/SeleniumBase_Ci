fa3b74288496795fe4311730f86d72877d03c05e
ImAcai <imacaiy@outlook.com>
2025-06-05 12:37:07 +0000

    update run_tests.sh.
    Signed-off-by: ImAcai <imacaiy@outlook.com>

--- run_tests.sh
+++ run_tests.sh
@@ -7,13 +7,12 @@ rm -rf venv reports latest_logs
 
 # 创建虚拟环境
 python3 -m venv venv
-source venv/bin/activate
 
-# 安装依赖
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
+# 安装依赖（使用虚拟环境中的 pip）
+./venv/bin/pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
 
-# 运行 SeleniumBase 测试并生成报告
-python -m seleniumbase run test_case/ \
+# 运行 SeleniumBase 测试（使用虚拟环境中的 python）
+./venv/bin/python -m seleniumbase run test_case/ \
   --browser=chrome \
   --headless \
   --junit-xml=reports/junit.xml \
