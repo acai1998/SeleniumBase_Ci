916423c97c6c5b48019a69fb563b92c01c8eff2b
ImAcai <imacaiy@outlook.com>
2025-06-07 14:43:54 +0000

    update run_tests.sh.
    Signed-off-by: ImAcai <imacaiy@outlook.com>

--- run_tests.sh
+++ run_tests.sh
@@ -2,17 +2,11 @@
 
 cd /var/jenkins_home/workspace/SeleniumBase-CI
 
-# 清理旧环境
-rm -rf venv reports latest_logs
-
-# 创建虚拟环境
-python3 -m venv venv
-
 # 安装依赖
-./venv/bin/pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
 
 # 执行 SeleniumBase 测试（注意：直接使用 seleniumbase CLI）
-./venv/bin/seleniumbase run test_case/ \
+pytest test_case/ \
   --browser=chrome \
   --headless \
   --junit-xml=reports/junit.xml \
