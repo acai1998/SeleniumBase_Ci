d830479760d4a69ecfc2c23430f1978a2489b026
ImAcai <imacaiy@outlook.com>
2025-06-13 09:46:10 +0000

    update run_tests.bat.
    Signed-off-by: ImAcai <imacaiy@outlook.com>

--- run_tests.bat
+++ run_tests.bat
@@ -1,14 +1,22 @@
 @echo off
-echo [Windows] Installing dependencies...
+setlocal
+echo [Windows] Upgrading pip...
 python -m pip install --upgrade pip
+
+echo [Windows] Installing dependencies from requirements.txt...
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
-pip install pytest pytest-html allure-pytest seleniumbase
+
+echo [Windows] Installing additional pytest plugins...
+pip install pytest pytest-html allure-pytest pytest-xdist
 
 echo [Windows] Running tests...
-python -m pytest test_case/ ^
+python -m pytest test_case ^
   --browser=chrome ^
   --dashboard --rs ^
   --headless ^
   --alluredir=allure-results ^
-  --junitxml=reports/junit.xml ^
-  --html=reports/report.html
+  --junitxml=reports\junit.xml ^
+  --html=reports\report.html ^
+  -n auto
+
+endlocal
